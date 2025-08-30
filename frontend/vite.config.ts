d2a030ea2629cfe786264e059ac9861122dc7f3f
Henry O <henryouly@gmail.com>
2025-08-30 23:50:50 +0000

    Setup tailwind css and shadcn UI

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
 import { defineConfig } from 'vite'
+import path from "path"
 import react from '@vitejs/plugin-react-swc'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/frontend/',
-  plugins: [react()],
+  plugins: [react(), tailwindcss()],
   build: {
     outDir: './dist',
     emptyOutDir: true
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
   }
 })
