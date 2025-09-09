52b5aa97296ec8a95fcddce609e1f8654c3cc839
Henry O <henryouly@gmail.com>
2025-09-09 05:41:29 +0000

    sidebar uses lucide solid icon

--- frontend/src/components/AppSidebar.tsx
+++ frontend/src/components/AppSidebar.tsx
@@ -1,14 +1,44 @@
 import type { Component } from 'solid-js';
 
+import { Film, ScrollText, Settings, Video } from 'lucide-solid';
+
+const items = [
+  {
+    title: "Live View",
+    url: "/live",
+    icon: Video,
+  },
+  {
+    title: "Recordings",
+    url: "/recordings",
+    icon: Film,
+  },
+  {
+    title: "Settings",
+    url: "/settings",
+    icon: Settings,
+  },
+  {
+    title: "Logs",
+    url: "/logs",
+    icon: ScrollText,
+  },
+]
+
 const AppSidebar: Component = () => {
   return (
     <div class="drawer-side">
       <label for="my-drawer-2" aria-label="close sidebar" class="drawer-overlay"></label>
       <ul class="menu bg-base-200 text-base-content min-h-full w-80 p-4">
-        <li><a href="/live">Live View</a></li>
-        <li><a href="/recordings">Recordings</a></li>
-        <li><a href="/logs">Logs</a></li>
-        <li><a href="/settings">Settings</a></li>
+        <li class="menu-title mb-2"><span>Sentryshot</span></li>
+        {items.map((item) => (
+          <li>
+            <a href={item.url}>
+              <item.icon class='w-4 h-4 mr-1' />
+              {item.title}
+            </a>
+          </li>
+        ))}
       </ul>
     </div>
   );
