e6d6185ffc47f268a07bd714e124d0736bde172a
Henry O <henryouly@gmail.com>
2025-09-09 04:08:38 +0000

    Redo project setup

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,8 @@
 /* @refresh reload */
+import './index.css';
 import { render } from 'solid-js/web';
 import 'solid-devtools';
 
-import './index.css';
 import App from './App';
 
 const root = document.getElementById('root');
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,11 @@
+import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
 import devtools from 'solid-devtools/vite';
-import path from "path";
+import path from 'path';
 
 export default defineConfig({
-  plugins: [devtools(), solidPlugin()],
+  plugins: [devtools(), solidPlugin(), tailwindcss()],
   build: {
     target: 'esnext',
     outDir: './dist',
